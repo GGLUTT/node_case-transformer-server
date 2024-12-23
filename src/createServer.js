1090691eae26243ad89be81c9d5093512e838009
luter <newgglutt@gmail.com>
2024-12-23 23:19:58 +0200

    deve

--- src/createServer.js
+++ src/createServer.js
@@ -1,15 +1,12 @@
-
+const { convertToCase } = require('./convertToCase');
 const http = require('http');
 
 function createServer() {
-  // Створення сервера
   const server = http.createServer((req, res) => {
     const url = new URL(req.url, `http://${req.headers.host}`);
     const textToConvert = url.pathname.slice(1);
     const toCase = url.searchParams.get('toCase');
-
     const supportedCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
-
     const errors = [];
 
     if (!textToConvert) {
@@ -41,66 +38,19 @@ function createServer() {
       return;
     }
 
-    let convertedText;
-    let originalCase;
-
-    if (/^[A-Z0-9_]+$/.test(textToConvert)) {
-      originalCase = 'UPPER';
-    } else if (/^[a-z0-9_]+$/.test(textToConvert)) {
-      originalCase = 'SNAKE';
-    } else if (/^[a-z0-9-]+$/.test(textToConvert)) {
-      originalCase = 'KEBAB';
-    } else if (/^[a-z][a-zA-Z0-9]+$/.test(textToConvert)) {
-      originalCase = 'CAMEL';
-    } else if (/^[A-Z][a-zA-Z0-9]+$/.test(textToConvert)) {
-      originalCase = 'PASCAL';
-    } else {
-      originalCase = 'UNKNOWN';
-    }
-
-    switch (toCase) {
-      case 'SNAKE':
-        convertedText = textToConvert
-          .replace(/([a-z])([A-Z])/g, '$1_$2')
-          .replace(/[-]/g, '_')
-          .toLowerCase();
-        break;
-      case 'KEBAB':
-        convertedText = textToConvert
-          .replace(/([a-z])([A-Z])/g, '$1-$2')
-          .replace(/[_]/g, '-')
-          .toLowerCase();
-        break;
-      case 'CAMEL':
-        convertedText = textToConvert
-          .replace(/[_-](\w)/g, (_, letter) => letter.toUpperCase())
-          .replace(/^[A-Z]/, (letter) => letter.toLowerCase());
-        break;
-      case 'PASCAL':
-        convertedText = textToConvert
-          .replace(/[_-](\w)/g, (_, letter) => letter.toUpperCase())
-          .replace(/^[a-z]/, (letter) => letter.toUpperCase());
-        break;
-      case 'UPPER':
-        convertedText = textToConvert.replace(/[_-]/g, '_').toUpperCase();
-        break;
-    }
+    const result = convertToCase(textToConvert, toCase);
 
     res.writeHead(200, { 'Content-Type': 'application/json' });
 
     res.end(
       JSON.stringify({
-        originalCase,
-        targetCase: toCase,
-        convertedText,
+        ...result,
         originalText: textToConvert,
+        targetCase: toCase,
       }),
     );
   });
 
-
   return server;
 }
-
-// Експортуємо функцію createServer
 module.exports = { createServer };
